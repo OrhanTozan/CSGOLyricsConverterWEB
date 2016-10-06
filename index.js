1ef06326b0107bcbcf9051dd10e47c1272b9d0a2
NahroTo <nahro.tozan@gmail.com>
2016-10-06 12:51:12 +0200

    changes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 $(document).ready(function()
 {
-	$(".convertBtn").click(function()
+	$("#convertBtn").click(function()
 	{
-		var selectedFile = document.getElementById("convertBtn").files[0];
-		console.log(selectedFile);
+		var filePath = $("#fileInput").val();
+		alert(filePath);
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", filePath, true);
+		xhr.onreadystatechange = function()
+		{
+			if (xhr.readyState == 4 && (xhr.statusCode == 400 || xhr.statusCode == 0))	
+			{
+				alert(filePath);
+			}
+		}
+		xhr.send(null);
 	});
 });
