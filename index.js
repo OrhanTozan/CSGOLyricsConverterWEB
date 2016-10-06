617964713fa7445d252a856936be5f723159846e
NahroTo <nahro.tozan@gmail.com>
2016-10-06 21:27:32 +0200

    title

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(document).ready(function()
 {
 	$("#convertBtn").click(function()
 	{
+		var convertedText = "alias nextLine \"l0\"";
 		var file = document.getElementById("fileInput").files[0];
 		var fileReader = new FileReader();
 		fileReader.readAsText(file);
