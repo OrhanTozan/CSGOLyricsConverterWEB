e17b12be7089e0df86634009e293b7698767d0de
NahroTo <nahro.tozan@gmail.com>
2016-10-06 23:34:35 +0200

    big changes

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 $(document).ready(function()
 {
+	var step = 1;
+	$(".step").click(function()
+	{
+		if (step == 1)
+		{
+			$(".temptext").val($(".rawtext").val());
+			$(".temptext").trigger('autoresize');
+			$(".row1").slideUp();
+			$(".row2").slideDown();
+			$(".row3").slideUp();
+			$(".rightcol").slideDown();
+		}
+		else if (step == 2)
+		{
+			$(".row1").slideUp();
+			$(".row2").slideUp();
+			$(".row3").slideDown();
+			$(this).fadeOut();
+		}
+		step++;
+	});
+
 	$("#convertBtn").click(function()
 	{
-		var convertedText = "alias nextLine \"l0\"";
-		var file = document.getElementById("fileInput").files[0];
-		var fileReader = new FileReader();
+		var file = document.getElementById("fileInput").files[0]; // GET FILE
+		var bindKey = "MOUSE3"; // GET BINDKEY
+		var convertedText = "alias nextLine \"l0\"";              // TEXT START
+		convertedText += "\r\nbind " + bindKey + " \"nextLine\"";
+		var fileReader = new FileReader(); // INIT FILEREADER
+
 		fileReader.readAsText(file);
 		fileReader.onload = function(event)
 		{
-			console.log(fileReader.result);
+			var rawLyrics = fileReader.result;
+			var lines = rawLyrics.split("\n");
+			for (let i = 0; i < lines.length; i++)
+			{
+				convertedText += "\r\nalias l" + i + " \"say " + lines[i] + ";alias nextLine l" + (i + 1) + "\"";
+			}
+			console.log(convertedText);
 		}
 	});
 });
