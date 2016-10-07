31d0a15ef411d6f415b4278e19e413b9e0b18412
NahroTo <nahro.tozan@gmail.com>
2016-10-07 19:16:32 +0200

    Made everything working now (stable)

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 $(document).ready(function()
 {
 	var step = 1;
+	var bindkey;
+	var rawlyrics;
+
+	$(function() {
+		$("form").submit(function() { return false; });
+	});
+
+	$('input, textarea').keypress(function(e){
+		if(e.keyCode==13)
+			$('.step').click();
+	});
+
 	$(".step").click(function()
 	{
 		if (step == 1)
 		{
-			$(".temptext").val($(".rawtext").val());
+			rawLyrics = $(".rawtext").val();	
+			$(".temptext").html(rawLyrics);
 			$(".temptext").trigger('autoresize');
 			$(".row1").slideUp();
 			$(".row2").slideDown();
@@ -14,6 +27,9 @@ $(document).ready(function()
 		}
 		else if (step == 2)
 		{
+			bindKey = $("#bindKey").val();
+			$(".temptext").html("<span style=\"color:yellow;\">bind " + bindKey + " \"nextLine\"</span>\r\n" + $(".temptext").html());
+			$(".temptext").trigger('autoresize');
 			$(".row1").slideUp();
 			$(".row2").slideUp();
 			$(".row3").slideDown();
@@ -24,22 +40,23 @@ $(document).ready(function()
 
 	$("#convertBtn").click(function()
 	{
-		var file = document.getElementById("fileInput").files[0]; // GET FILE
-		var bindKey = "MOUSE3"; // GET BINDKEY
-		var convertedText = "alias nextLine \"l0\"";              // TEXT START
-		convertedText += "\r\nbind " + bindKey + " \"nextLine\"";
-		var fileReader = new FileReader(); // INIT FILEREADER
-
-		fileReader.readAsText(file);
-		fileReader.onload = function(event)
+		var lines = rawLyrics.split("\n");
+		var scriptedLyrics = "<span style=\"color:yellow;\">alias nextLine \"l0\"</span>";
+		for (let i = 0; i < lines.length; i++)
 		{
-			var rawLyrics = fileReader.result;
-			var lines = rawLyrics.split("\n");
-			for (let i = 0; i < lines.length; i++)
-			{
-				convertedText += "\r\nalias l" + i + " \"say " + lines[i] + ";alias nextLine l" + (i + 1) + "\"";
-			}
-			console.log(convertedText);
+			scriptedLyrics += "\r\n<span style=\"color:yellow;\">alias l" + i + " \"say </span>" + lines[i] + "<span style=\"color:yellow;\">;alias nextLine l" + (i + 1) + "\"</span>";
 		}
+		$(".temptext").html("<span style=\"color:yellow;\">bind " + bindKey + " \"nextLine\"</span>\r\n" + scriptedLyrics);
+		
+		
+		setTimeout(function()
+		{
+			$(".leftcol").slideUp(400, function()
+			{
+				$(".rightcol").removeClass("offset-s1");
+				$(".rightcol").removeClass("hide-on-med-and-down");
+				$("#temptextTitle").text("Result");
+			});
+		});
 	});
 });
